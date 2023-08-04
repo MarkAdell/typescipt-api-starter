3e2ec3bfc3a22b659c7382cbe156867d82ec212c
MarkAdell <mark.adell@outlook.com>
2023-08-04 21:39:38 +0300

    add max_memory_restart to pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,6 +3,7 @@ module.exports = {
     {
       name: 'app',
       script: 'dist/index.js',
+      max_memory_restart: '1G', // restart if process uses more than 1G memory
       env_production: {
         NODE_ENV: 'production'
       },
