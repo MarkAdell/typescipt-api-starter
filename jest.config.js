0a89d6d3f41727a18568c014c8b15e79ec6088ef
MarkAdell <mark.adell@outlook.com>
2023-08-04 21:32:56 +0300

    remove testMatch from jest configs as these are the defaults anyways

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,6 @@ module.exports = {
   roots: [
     '<rootDir>/src',
   ],
-  testMatch: [
-    '**/__tests__/**/*.+(ts|tsx|js)',
-    '**/?(*.)+(spec|test).+(ts|tsx|js)',
-  ],
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
   },
